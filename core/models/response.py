cb7c286736b625a2b63add3ff3e3c2b71c3fbe14
Saul Hernandez <saul7.hernandez@gmail.com>
2023-10-24 13:05:22 -0400

    update

--- core/models/response.py
+++ core/models/response.py
@@ -5,8 +5,8 @@ from django.utils.timezone import now
 class HeartbeatRequestLog(Model):
     url = CharField(max_length=200)
     timestamp = DateTimeField(default=now)
-    status: PositiveIntegerField(default=200)
-    elapsed: TimeField()
+    status = PositiveIntegerField(default=200)
+    elapsed = TimeField()
     error = CharField(max_length=100, null=True, blank=True)
     reason = CharField(max_length=100, null=True, blank=True)
 
