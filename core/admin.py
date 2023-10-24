3d9827c4356773fc469795f02e66a9e716891650
Saul Hernandez <saul7.hernandez@gmail.com>
2023-10-24 12:57:36 -0400

    update

---
+++ core/admin.py
@@ -0,0 +1,10 @@
+from django.contrib.admin import ModelAdmin, register
+
+from core.models import HeartbeatRequestLog
+
+
+@register(HeartbeatRequestLog)
+class HeartbeatRequestLogAdmin(ModelAdmin):
+    list_display = ["url", "timestamp", "status", "elapsed", "error", "reason"]
+    search_fields = ["url", "error", "reason"]
+    list_filter = ["status", "error"]
