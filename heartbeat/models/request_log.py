aa743c329647d3bd8c71791d4b59fbf64dc5cc14
Saul Hernandez <saul7.hernandez@gmail.com>
2023-10-24 16:04:41 -0400

    update

--- heartbeat/models/request_log.py
+++ heartbeat/models/request_log.py
@@ -1,5 +1,4 @@
-from django.db.models import CharField, DateTimeField, PositiveIntegerField, TimeField, Model, FloatField
-from django.forms import DecimalField
+from django.db.models import CharField, DateTimeField, PositiveIntegerField, Model, DecimalField
 from django.utils.timezone import now
 
 
@@ -7,7 +6,7 @@ class HeartbeatRequestLog(Model):
     url = CharField(max_length=200)
     timestamp = DateTimeField(default=now)
     status = PositiveIntegerField(default=200)
-    elapsed = DecimalField(decimal_places=14)
+    elapsed = DecimalField(decimal_places=14, max_digits=32)
     error = CharField(max_length=100, null=True, blank=True)
     reason = CharField(max_length=100, null=True, blank=True)
 
