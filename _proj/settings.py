bcd54391572cdd4b002099467e9f00756d808d40
Saul Hernandez <saul7.hernandez@gmail.com>
2023-10-25 15:30:02 -0400

    update

--- _proj/settings.py
+++ _proj/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ROOT_DIR = environ.Path(__file__) - 2
+env = environ.Env()
+env.read_env(str(ROOT_DIR.path(".env")))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -74,12 +77,8 @@ WSGI_APPLICATION = "_proj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 
 # Password validation
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="heartbeat",
-    version="1.0.0",
+    version="0.1.0",
     description="Heartbeat",
     author="Saúl Hernández",
     author_email="saul@teamt.benefi365.com",
